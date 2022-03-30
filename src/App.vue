9253fbf69bc4ecbb60df719da074c7c17aa7ce11
salissadiq <salissadiq@gmail.com>
2022-03-30 10:53:35 +0100

    Project initialization

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navbar container">
+        <a href="" class="logo">Allon KanBan</a>
+    <div class="nav-link">
+        <a href="">home</a>
+        <a href="">fact</a>
+        <a href="">about</a>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    p{
+        color: red
+    }
+</style>
