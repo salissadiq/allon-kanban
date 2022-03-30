617d657bb8607f5000a6e24b87e521de039526b8
salissadiq <salissadiq@gmail.com>
2022-03-30 11:44:15 +0100

    Navbar style and create views folder with three file

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="navbar container">
         <a href="" class="logo">Allon KanBan</a>
-    <div class="nav-link">
+    <div class="nav-links">
         <a href="">home</a>
         <a href="">fact</a>
         <a href="">about</a>
+        <button class="btn">
+          <i class="fas fa-plus"></i>  add board
+        </button>
     </div>
     </div>
 </template>
